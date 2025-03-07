5f9ceab28a11bc57b3265ec6f3763dfc98c8bc49
sudarshanreddyc <sudarshanreddy.c123@gmail.com>
2025-03-06 22:03:33 -0500

    Updated experiences

--- client/src/Modules/Experience/Experience_BusinessLogic.js
+++ client/src/Modules/Experience/Experience_BusinessLogic.js
@@ -26,8 +26,8 @@ export class Experience_BusinessLogic {
           company: "Insightsoftware",
           logo: process.env.PUBLIC_URL + "/assets/Insightsoftware.png",
           responsibilities: `
-            • Migrated an analytical tool from Knockout.js to React with .NET and SQL backend, improving performance by 25% and maintainability, implemented encoding/decoding in .NET to secure sensitive data, and optimized CI/CD pipelines with Azure DevOps, reducing deployment times by 30%.
-          `,
+                • Migrated an analytical tool from a legacy framework to a modern front-end framework, improving performance by 25% and maintainability. Implemented data encoding/decoding to secure sensitive data and optimized deployment pipelines, reducing deployment times by 30%.
+            `,
           fromDate: "01/2024",
           toDate: "07/2024",
         },
@@ -37,8 +37,8 @@ export class Experience_BusinessLogic {
           company: "Arcadix Infotech Pvt Ltd",
           logo: process.env.PUBLIC_URL + "/assets/Arcadix.jpg",
           responsibilities: `
-            • Proposed and led the migration from Angular.js to React for the School Management System, improving performance by 30% and reducing technical debt, integrated SignalR for real-time updates of students' marks, and developed scalable .NET and SQL Server web APIs with microservice architecture, integrating Elasticsearch to improve search performance by 40% and system reliability.
-          `,
+                • Proposed and led the migration of a School Management System from an older framework to a modern front-end framework, improving performance by 30% and reducing technical debt. Integrated real-time updates for student marks and developed scalable APIs with microservices, improving search performance by 40% and system reliability.
+            `,
           fromDate: "12/2021",
           toDate: "01/2024",
         },
@@ -48,8 +48,8 @@ export class Experience_BusinessLogic {
           company: "Odessa Technologies",
           logo: process.env.PUBLIC_URL + "/assets/Odessa.png",
           responsibilities: `
-            • Contributed to the development of LeaseWave, a global lease management system, by optimizing backend processes with .NET Core, C#, React, and Dapper, improved SQL query efficiency to reduce database response times by 15%, streamlined CI/CD pipelines using Azure DevOps, and practiced TDD with NUnit and Jest, enhancing code quality and reducing bugs by 25%.
-          `,
+                • Contributed to the development of a global lease management system by optimizing backend processes, improving query efficiency to reduce database response times by 15%, streamlining deployment pipelines, and practicing test-driven development to enhance code quality and reduce bugs by 25%.
+            `,
           fromDate: "07/2020",
           toDate: "11/2021",
         },
@@ -59,8 +59,8 @@ export class Experience_BusinessLogic {
           company: "Odessa Technologies",
           logo: process.env.PUBLIC_URL + "/assets/Odessa.png",
           responsibilities: `
-            • Optimized backend processes for LeaseWave, a global lease management system, using .NET Core, C#, React, and Dapper, enhancing performance and scalability; improved SQL query efficiency, reducing database response times by 15%, and streamlined CI/CD pipelines with Azure DevOps, boosting team productivity and deployment reliability.
-          `,
+                • Optimized backend processes for a global lease management system, enhancing performance and scalability; improved query efficiency, reducing database response times by 15%, and streamlined deployment pipelines, boosting team productivity and deployment reliability.
+            `,
           fromDate: "09/2019",
           toDate: "06/2020",
         },
